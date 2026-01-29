052487d1b97a854c498bcd3bf8ea78a77c7eb1c2
MaxiHawk <maxihawk@gmail.com>
2026-01-29 16:55:04 -0300

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -43,3 +43,73 @@ def generar_loot():
         return "Raro", {"AP": 40, "MP": 10, "VP": 0}, "💼"
     else: # 50% Común
         return "Común", {"AP": 20, "MP": 5, "VP": 0}, "📦"
+# --- 🛠️ HELPER FUNCTIONS (HERRAMIENTAS MAESTRAS DE NOTION) ---
+import re
+
+def get_notion_text(props, name, default=""):
+    """Extrae texto plano de propiedades 'title' o 'rich_text'."""
+    try:
+        prop = props.get(name, {})
+        # Soporta tanto 'title' como 'rich_text'
+        content_list = prop.get("title", []) or prop.get("rich_text", [])
+        if content_list:
+            return "".join([t.get("plain_text", "") for t in content_list])
+        return default
+    except: return default
+
+def get_notion_number(props, name, default=0):
+    """Extrae números de propiedades 'number'."""
+    try:
+        val = props.get(name, {}).get("number")
+        return val if val is not None else default
+    except: return default
+
+def get_notion_select(props, name, default=None):
+    """Extrae el nombre de una opción 'select'."""
+    try:
+        val = props.get(name, {}).get("select")
+        return val.get("name", default) if val else default
+    except: return default
+
+def get_notion_multi_select(props, name, default_list=None):
+    """Extrae una lista de nombres de 'multi_select'."""
+    if default_list is None: default_list = []
+    try:
+        val_list = props.get(name, {}).get("multi_select", [])
+        return [item.get("name") for item in val_list] if val_list else default_list
+    except: return default_list
+
+def get_notion_date(props, name, default=None):
+    """Extrae la fecha de inicio de una propiedad 'date'."""
+    try:
+        val = props.get(name, {}).get("date")
+        return val.get("start", default) if val else default
+    except: return default
+
+def get_notion_url(props, name, default="#"):
+    """Extrae URL de propiedades 'url'."""
+    try:
+        return props.get(name, {}).get("url", default)
+    except: return default
+
+def get_notion_checkbox(props, name, default=False):
+    """Extrae valor booleano de 'checkbox'."""
+    try:
+        return props.get(name, {}).get("checkbox", default)
+    except: return default
+
+def get_notion_file_url(props, name, default=None):
+    """Extrae la URL del primer archivo en propiedades 'files'."""
+    try:
+        files = props.get(name, {}).get("files", [])
+        if files:
+            # Notion usa 'file' para internos y 'external' para externos
+            return files[0].get("file", {}).get("url") or files[0].get("external", {}).get("url")
+        return default
+    except: return default
+
+def validar_codigo_seguro(codigo):
+    """Valida formato seguro (A-Z, 0-9, -)."""
+    if not codigo: return False
+    codigo = codigo.strip().upper()
+    return bool(re.match(r'^[A-Z0-9-]+$', codigo))
